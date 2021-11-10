974a6e23470ef430b352c6e288be72d2d039e303
sameerbintaher <sameerbintaher@gmail.com>
2021-11-10 23:53:51 +0600

    d-2-1

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWy4IhuYbHCy8eVf4D8DQGszlYUbu7yHo",
+  authDomain: "sam-motors.firebaseapp.com",
+  projectId: "sam-motors",
+  storageBucket: "sam-motors.appspot.com",
+  messagingSenderId: "123136569344",
+  appId: "1:123136569344:web:cb2fde0ac259b78082a8e5",
+};
+
+export default firebaseConfig;
